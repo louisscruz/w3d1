462e47bc152a37d57d70beb0d9f2ec78f6d86f42
App Academy Student <student@appacademy.io>
2016-09-26 17:59:12 -0700

    Start 09

--- lib/09_self_joins.rb
+++ lib/09_self_joins.rb
@@ -17,40 +17,63 @@ require_relative './sqlzoo.rb'
 def num_stops
   # How many stops are in the database?
   execute(<<-SQL)
+  SELECT
+    COUNT(*)
+  FROM
+    stops
   SQL
 end
 
 def craiglockhart_id
   # Find the id value for the stop 'Craiglockhart'.
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    stops
+  WHERE
+    name = 'Craiglockhart'
   SQL
 end
 
 def lrt_stops
   # Give the id and the name for the stops on the '4' 'LRT' service.
   execute(<<-SQL)
+  SELECT
+    stops.id, stops.name
+  FROM
+    stops
+  JOIN
+    routes
+  ON
+    stops.id = routes.stop_id
+  WHERE
+    routes.company = 'LRT' AND routes.num = '4'
   SQL
 end
 
 def connecting_routes
   # Consider the following query:
   #
-  # SELECT
-  #   company,
-  #   num,
-  #   COUNT(*)
-  # FROM
-  #   routes
-  # WHERE
-  #   stop_id = 149 OR stop_id = 53
-  # GROUP BY
-  #   company, num
+
   #
   # The query gives the number of routes that visit either London Road
   # (149) or Craiglockhart (53). Run the query and notice the two services
   # that link these stops have a count of 2. Add a HAVING clause to restrict
-  # the output to these two routes.
+  # the output to tshese two routes.
   execute(<<-SQL)
+  SELECT
+   company,
+   num,
+   COUNT(*)
+  FROM
+   routes
+  WHERE
+   stop_id = 149 OR stop_id = 53
+  GROUP BY
+   company, num
+  HAVING
+  
   SQL
 end
 
